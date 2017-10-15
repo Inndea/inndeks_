79530ba7379110557baa4be954d7486a4dd23228
OKAN ERDOGAN <okanerdogan@OKANs-Mac.local>
2017-10-15 22:03:44 +0300

    Initial Commit

---
+++ InndeksTests/InndeksTests.swift
@@ -0,0 +1,36 @@
+//
+//  InndeksTests.swift
+//  InndeksTests
+//
+//  Created by OKAN ERDOGAN on 15.10.2017.
+//  Copyright © 2017 Inndea. All rights reserved.
+//
+
+import XCTest
+@testable import Inndeks
+
+class InndeksTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
